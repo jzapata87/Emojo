73c748ac637c827a51e72ab25b4da8d9273013a8
Javier Zapata <zapataj87@gmail.com>
2019-03-24 10:39:45 -0500

    added image picker

--- src/screens/UploadPhotoScreen.js
+++ src/screens/UploadPhotoScreen.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
-import { View, Text, Button, SafeAreaView } from 'react-native';
+import {StyleSheet, Text, View, Button, Image} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 import { createStackNavigator } from 'react-navigation';
 
-const UploadPhotoScreen = () => (
-  <SafeAreaView >
-    <Text> hi</Text>
+// const UploadPhotoScreen = () => (
+//   <SafeAreaView >
+//     <Text> hi</Text>
+//
+//   </SafeAreaView>
+// )
 
-  </SafeAreaView>
-)
+class UploadPhotoScreen extends Component{
+
+  state = {
+    photo: null,
+  }
+
+  handleChoosePhoto = () => {
+    const options = {
+      noData: true,
+    }
+    ImagePicker.launchImageLibrary(options, response => {
+      if (response.uri) {
+        this.setState({ photo: response })
+      }
+    })
+  }
+
+  render() {
+    const { photo } = this.state
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {photo && (
+          <React.Fragment>
+            <Image
+              source={{ uri: photo.uri }}
+              style={{ width: 300, height: 300 }}
+            />
+            {/* <Button title="Upload" onPress={this.handleUpload} /> */}
+          </React.Fragment>
+        )}
+        <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
 export default createStackNavigator({
     Home: {
